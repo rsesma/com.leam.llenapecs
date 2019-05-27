55601809b8083d41bcd3da7845924420e299e4c4
R Sesma <rsesma@metodo.uab.cat>
2019-05-27 19:43:31 +0200

    v2019.05.27

--- src/com/leam/llenapecs/LlenaPECs.java
+++ src/com/leam/llenapecs/LlenaPECs.java
@@ -2,6 +2,12 @@ package com.leam.llenapecs;
 
 import java.awt.FileDialog;
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -23,6 +29,7 @@ public class LlenaPECs {
     	
     	Boolean lContinue = true;
         JFrame frame = new JFrame("JFileChooser dialog window");        			// JFrame for the dialogs
+    	Boolean lMessage = true;
     	
     	if (args.length == 0) {
 	    	FileDialog dlg = new FileDialog(frame, "Indicar la PEC", FileDialog.LOAD);
@@ -34,6 +41,8 @@ public class LlenaPECs {
 	    	else dir = dlg.getDirectory() + dlg.getFile();
     	} else {
     		dir = args[0];
+    		if (args.length > 1) lMessage = args[1].equalsIgnoreCase("verbose");
+    		else lMessage = false;
     	}
     	
     	if (lContinue) {
@@ -44,23 +53,63 @@ public class LlenaPECs {
 			    
 				// Get field names
 				List<PDField> fields = form.getFields();
+				List<String> pnames = new ArrayList<String>();
+				List<String> mnames = new ArrayList<String>();
 				for (PDField f : fields) {
 					String name = f.getFullyQualifiedName();
                 	// ignore APE1, APE2, NOMBRE, DNI, HONOR, COMENT fields
                 	if (!name.equalsIgnoreCase("APE1") & !name.equalsIgnoreCase("APE2") & !name.equalsIgnoreCase("NOMBRE") &
-                		!name.equalsIgnoreCase("DNI") & !name.equalsIgnoreCase("HONOR") & !name.equalsIgnoreCase("COMENT")) {
+                		!name.equalsIgnoreCase("DNI") & !name.equalsIgnoreCase("HONOR") & !name.equalsIgnoreCase("COMENT") &
+                		(name.substring(0,1).equalsIgnoreCase("P") | name.substring(0,1).equalsIgnoreCase("M"))) {
+                		
+                		if (name.substring(0,1).equalsIgnoreCase("P")) pnames.add(name);
+                		if (name.substring(0,1).equalsIgnoreCase("M")) mnames.add(name);
+                		
+                		String value = "";
+                		
                 		if (f instanceof PDTextField) {
-                			System.out.println(name + ": text");                			
+                			PDTextField e = (PDTextField) f;				// text field: numeric or memo
+                			int max = e.getMaxLen();
+							if (max <= 10) value = "1234567890".substring(0,max-1);				// for short (numeric) fields, 1234567890 text according to field length 
+							else if (max <=1000) value = String.join("", Collections.nCopies(max, "a"));	// for long (memo) fields, repeated "a" until maximum length
                 		}
-                		else if (f instanceof PDComboBox) {
-                			System.out.println(name + ": combo");                			
+                		if (f instanceof PDComboBox) {
+                			PDComboBox c = (PDComboBox) f;					// combobox field: closed answer
+                			List<String> opts = c.getOptions();
+                			value = opts.get(opts.size()-1);
                 		}
-                		else {
-                			System.out.println(name + ": unknown");
-                		}
+                		
+                		if (value.length()>0) f.setValue(value);			// set the field value (fill)
                 	}
 				}
+				pdf.save(dir.replace(".pdf","_campos.pdf"));				// save pdf as a copy
+				pdf.close();
+				form = null;
+				pdf = null;
 				
+				// sort p and m names
+				Collections.sort(pnames);
+				Collections.sort(mnames);
+				List<String> names = new ArrayList<String>();				
+				for (String p : pnames) {
+					names.add(p);
+					int i = Integer.parseInt(p.substring(0,p.indexOf("_")).replace("P",""));
+					for (String m : mnames) {
+						int j = Integer.parseInt(m.substring(0,m.indexOf("_")).replace("M",""));
+						if (i == j) names.add(m);
+					}
+				}
+				// write field names to txt
+				Path fdata = Paths.get(dir.replace(".pdf","_campos.txt"));
+				Files.write(fdata, names, Charset.forName("UTF-8"));
+				
+				if (lMessage) {
+					String newline = System.getProperty("line.separator");
+					JOptionPane.showMessageDialog(frame,"Proceso finalizado." + newline + newline +
+							"Se han creado los archivos:" + newline +
+							dir.replace(".pdf","_campos.pdf") + newline +
+							dir.replace(".pdf","_campos.txt"),"Llena PECs",JOptionPane.INFORMATION_MESSAGE);					
+				}
     		} catch (Exception e) {
     			JOptionPane.showMessageDialog(frame,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
     		}
