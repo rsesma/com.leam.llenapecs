c0872b5d2220232d1c3c7f0a42005ee192aed07e
R Sesma <rsesma@metodo.uab.cat>
2020-01-13 14:26:49 +0100

    v2020.01.13 (2): corregir bug ancho campos

--- src/com/leam/llenapecs/LlenaPECs.java
+++ src/com/leam/llenapecs/LlenaPECs.java
@@ -71,7 +71,7 @@ public class LlenaPECs {
                 			PDTextField e = (PDTextField) f;				// text field: numeric or memo
                 			int max = e.getMaxLen();
                 			if (max>0) {
-								if (max <= 10) value = "1234567890".substring(0,max-1);				// for short (numeric) fields, 1234567890 text according to field length 
+								if (max <= 10) value = "1234567890".substring(0,max);				// for short (numeric) fields, 1234567890 text according to field length 
 								else if (max <=1000) value = String.join("", Collections.nCopies(max, "a"));	// for long (memo) fields, repeated "a" until maximum length
                 			}
                 		}
