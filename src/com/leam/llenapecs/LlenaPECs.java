f85f7f878de5dfd26b9d0710d4df11b9164e633f
rsesma <38663344+rsesma@users.noreply.github.com>
2021-05-21 13:51:39 +0200

    v.2021.05.21: rellenar y verificar fijos, duplicados, desactivados

--- src/com/leam/llenapecs/LlenaPECs.java
+++ src/com/leam/llenapecs/LlenaPECs.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -15,6 +16,7 @@ import javax.swing.JOptionPane;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox;
 import org.apache.pdfbox.pdmodel.interactive.form.PDComboBox;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
@@ -33,7 +35,13 @@ public class LlenaPECs {
     	
     	if (args.length == 0) {
 	    	FileDialog dlg = new FileDialog(frame, "Indicar la PEC", FileDialog.LOAD);
-	    	dlg.setDirectory(System.getProperty("user.home"));			// default dir
+	    	String defdir = "";				// default dir: the jar folder
+	    	try {
+	    		defdir = new File(LlenaPECs.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+	    	} catch (Exception e) {
+    			defdir = System.getProperty("user.home");		// if not available, user home directory
+    		}
+	    	dlg.setDirectory(defdir);
 	    	dlg.setFile("*.pdf");										// filter: PDF files only
 	    	dlg.setVisible(true);
 	    	String filename = dlg.getFile();
@@ -55,6 +63,7 @@ public class LlenaPECs {
 				List<PDField> fields = form.getFields();
 				List<String> pnames = new ArrayList<String>();
 				List<String> mnames = new ArrayList<String>();
+				List<String> readonly = new ArrayList<String>();
 				for (PDField f : fields) {
 					String name = f.getFullyQualifiedName();
                 	// ignore APE1, APE2, NOMBRE, DNI, HONOR, COMENT fields
@@ -62,8 +71,12 @@ public class LlenaPECs {
                 		!name.equalsIgnoreCase("DNI") & !name.equalsIgnoreCase("HONOR") & !name.equalsIgnoreCase("COMENT") &
                 		(name.substring(0,1).equalsIgnoreCase("P") | name.substring(0,1).equalsIgnoreCase("M"))) {
                 		
+                		if (name.indexOf("_") != name.lastIndexOf("_")) name = name.substring(0,name.lastIndexOf("_"));
+                		
                 		if (name.substring(0,1).equalsIgnoreCase("P")) pnames.add(name);
                 		if (name.substring(0,1).equalsIgnoreCase("M")) mnames.add(name);
+                		
+                		if (f.isReadOnly()) readonly.add(name);
                 		
                 		String value = "";
                 		
@@ -71,8 +84,10 @@ public class LlenaPECs {
                 			PDTextField e = (PDTextField) f;				// text field: numeric or memo
                 			int max = e.getMaxLen();
                 			if (max>0) {
-								if (max <= 10) value = "1234567890".substring(0,max);				// for short (numeric) fields, 1234567890 text according to field length 
-								else if (max <=1000) value = String.join("", Collections.nCopies(max, "a"));	// for long (memo) fields, repeated "a" until maximum length
+                				// for short (numeric) fields, 1234567890 text according to field length
+								if (max <= 10) value = "1234567890".substring(0,max);				 
+								// for long (memo) fields, repeated "a" until maximum length
+								else if (max <=1000) value = String.join("", Collections.nCopies(max, "a"));	
                 			}
                 		}
                 		if (f instanceof PDComboBox) {
@@ -84,7 +99,28 @@ public class LlenaPECs {
                 		if (value.length()>0) f.setValue(value);			// set the field value (fill)
                 	}
 				}
-				pdf.save(dir.replace(".pdf","_campos.pdf"));				// save pdf as a copy
+				
+				// check for fixed fields
+				String fixed = "";
+				for (String n : "APE1 APE2 NOMBRE DNI HONOR COMENT".split(" ")) {
+			    	try {
+			    		if (n.equalsIgnoreCase("HONOR")) {
+			    			PDCheckBox ch = (PDCheckBox) form.getField(n);
+			    			ch.check();
+			    		} else {
+				    		PDTextField field = (PDTextField) form.getField(n);
+				    		if (n.equalsIgnoreCase("APE1")) field.setValue("PRIMER APELLIDO");
+				    		if (n.equalsIgnoreCase("APE2")) field.setValue("SEGUNDO APELLIDO");
+				    		if (n.equalsIgnoreCase("NOMBRE")) field.setValue("NOMBRE");
+				    		if (n.equalsIgnoreCase("DNI")) field.setValue("12345678X");
+				    		if (n.equalsIgnoreCase("COMENT")) field.setValue("COMENTARIO");			    			
+			    		}
+			    	} catch (Exception e) {
+			    		fixed = fixed + (fixed.length()>0 ? ";" : "") + n;
+		    		}
+				}
+				// save pdf as a copy
+				pdf.save(dir.replace(".pdf","_campos.pdf"));
 				pdf.close();
 				form = null;
 				pdf = null;
@@ -92,8 +128,10 @@ public class LlenaPECs {
 				// sort p and m names
 				Collections.sort(pnames);
 				Collections.sort(mnames);
-				List<String> names = new ArrayList<String>();				
+				List<String> names = new ArrayList<String>();
+				HashSet<String> dup = new HashSet<String>();
 				for (String p : pnames) {
+					if (names.contains(p)) dup.add(p);
 					names.add(p);
 					int i = Integer.parseInt(p.substring(0,p.indexOf("_")).replace("P",""));
 					for (String m : mnames) {
@@ -101,16 +139,25 @@ public class LlenaPECs {
 						if (i == j) names.add(m);
 					}
 				}
+				
 				// write field names to txt
 				Path fdata = Paths.get(dir.replace(".pdf","_campos.txt"));
 				Files.write(fdata, names, Charset.forName("UTF-8"));
 				
+				String newline = System.getProperty("line.separator");
 				if (lMessage) {
-					String newline = System.getProperty("line.separator");
 					JOptionPane.showMessageDialog(frame,"Proceso finalizado." + newline + newline +
 							"Se han creado los archivos:" + newline +
 							dir.replace(".pdf","_campos.pdf") + newline +
 							dir.replace(".pdf","_campos.txt"),"Llena PECs",JOptionPane.INFORMATION_MESSAGE);					
+				}
+				
+				if (fixed.length()>0 | !dup.isEmpty() | !readonly.isEmpty()) {
+					JOptionPane.showMessageDialog(frame,
+							(fixed.length()>0 ? "Falta(n) campo(s) fijo(s): " + fixed + newline + newline : "" ) +
+							(!dup.isEmpty() ? "Hay nombres duplicados: " + dup.toString() + newline + newline : "" ) +
+							(!readonly.isEmpty() ? "Hay campos desactivados: " + readonly.toString() : "" ),
+							"Llena PECs",JOptionPane.WARNING_MESSAGE);
 				}
     		} catch (Exception e) {
     			JOptionPane.showMessageDialog(frame,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
